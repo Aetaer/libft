c9c0f7108fe4ae1d2d5e2a012fac97f8bae5973c
Aetaer <andrej.kapitonov@outlook.com>
2023-02-05 13:51:29 +0100

    almost final commit

--- libft.h
+++ libft.h
@@ -4,6 +4,15 @@
 #include <unistd.h>
 #include <stdio.h>
 
+void				ft_bzero(void *s, size_t n);
+
+void				*ft_memset(void *b, int c, size_t len);
+void				*ft_memcpy(void *dst, const void *src, size_t n);
+void				*ft_memccpy(void *dst, const void *src, int c, size_t n);
+void				*ft_memmove(void *dst, const void *src, size_t len);
+void				*ft_memchr(const void *s, int c, size_t n);
+void				*ft_calloc(size_t count, size_t size);
+
 int ft_isalpha(int c);
 int ft_isdigit(int c);
 int ft_isalnum(int c);
